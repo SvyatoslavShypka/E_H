b49fca0cc6a6010b8917fe5ec2d0d22bc9e61c13
Svyatoslav Shypka <svyat200909@gmail.com>
2020-10-07 18:55:30 +0200

    Initial commit

---
+++ com/javarush/task/task20/task2001/Solution.java
@@ -0,0 +1,140 @@
+package com.javarush.task.task20.task2001;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/* 
+Читаем и пишем в файл: Human
+*/
+public class Solution {
+    public static void main(String[] args) {
+        //исправьте outputStream/inputStream в соответствии с путем к вашему реальному файлу
+        try {
+            File your_file_name = File.createTempFile("2001_data", ".txt",new File("C:\\Private\\JR_projects\\JavaRushTasks\\test_files"));
+//            File your_file_name = File.createTempFile("2001_data",null);
+            OutputStream outputStream = new FileOutputStream(your_file_name);
+            InputStream inputStream = new FileInputStream(your_file_name);
+
+            Human ivanov = new Human("Ivanov", new com.javarush.task.task20.task2001.Asset("home", 999_999.99), new Asset("car", 2999.99));
+//            Human ivanov = new Human("Ivanov", null);
+            ivanov.save(outputStream);
+            outputStream.flush();
+
+            Human somePerson = new Human();
+//            Human somePerson = new Human("Ivanov", new Asset("home", 999_999.98), new Asset("car", 2999.99));
+            somePerson.load(inputStream);
+            inputStream.close();
+            //check here that ivanov equals to somePerson - проверьте тут, что ivanov и somePerson равны
+//            System.out.println(ivanov.getClass() + " : " + somePerson.getClass());
+            System.out.println(ivanov.equals(somePerson));
+//            System.out.println(ivanov.hashCode() + " : " + somePerson.hashCode());
+
+        } catch (IOException e) {
+            //e.printStackTrace();
+            System.out.println("Oops, something wrong with my file");
+        } catch (Exception e) {
+            //e.printStackTrace();
+            System.out.println("Oops, something wrong with save/load method");
+        }
+    }
+
+    public static class Human {
+        public String name;
+        public List<Asset> assets = new ArrayList<>();
+
+        public Human() {
+        }
+
+        public Human(String name, Asset... assets) {
+            this.name = name;
+            if (assets != null) {
+                this.assets.addAll(Arrays.asList(assets));
+            }
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Human human = (Human) o;
+
+            if (name != null ? !name.equals(human.name) : human.name != null) return false;
+            return assets != null ? assets.equals(human.assets) : human.assets == null;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = name != null ? name.hashCode() : 0;
+            result = 31 * result + (assets != null ? assets.hashCode() : 0);
+            return result;
+        }
+
+        public void save(OutputStream outputStream) throws Exception {
+            //implement this method - реализуйте этот метод
+            String isAssetsPresent = !assets.isEmpty() ? "Yes" : "No";
+            outputStream.write((name + "\n").getBytes());
+            outputStream.write((isAssetsPresent + "\n").getBytes());
+            if (!assets.isEmpty()) {
+                assets.forEach((Asset x) -> {
+                    try {
+                        outputStream.write((x.getName() + "\n").getBytes());
+                        outputStream.write((x.getPrice() + "\n").getBytes());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+            }
+//            outputStream.flush();
+        }
+
+        public void load(InputStream inputStream) throws Exception {
+            //implement this method - реализуйте этот метод
+//            byte[] buffer = new byte[inputStream.available()];
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
+                //            try (BufferedReader br = new BufferedReader(new InputStreamReader(new ByteArrayInputStream(buffer,
+//                    0, inputStream.read(buffer))))) {
+//                ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(buffer);
+                StringBuilder s = new StringBuilder();
+                //                System.out.println(br.readLine());
+                String k;
+                while((k = br.readLine()) != null)
+
+                {
+                    s.append(k + "\n");
+                }
+//                System.out.println(s.toString());
+                /*
+                                while (inputStream.available() > 0) {
+                                    System.out.println(buffer[0]);
+                                    s.append((char) inputStream.read());
+                                }
+                */
+                String str;
+                String[] data;
+                str = String.valueOf(s);
+                data = str.split("\n");
+                name = data[0];
+//            System.out.println(data[5]);
+            if("Yes".equals(data[1])) {
+                    for (int i = 2; i < data.length; i += 2) {
+                        assets.add(new Asset(data[i], Double.parseDouble(data[i + 1])));
+                    }
+                }
+            }
+        }
+
+        public String toString() {
+            StringBuilder sb = new StringBuilder(name + "\n");
+            for (Asset asset : assets) {
+                sb.append(asset.getName() + " : " + asset.getPrice() + "\n");
+            }
+            return sb.toString();
+        }
+    }
+    public static class H1 extends Human {
+
+    }
+}
